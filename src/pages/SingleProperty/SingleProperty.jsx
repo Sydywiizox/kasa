419aac405c06539598ea146894c746d570b30cb3
Sydywiizox <andrea_duhamel@outlook.fr>
2024-12-29 01:31:48 +0100

    Redirect wrong single id

--- src/pages/SingleProperty/SingleProperty.jsx
+++ src/pages/SingleProperty/SingleProperty.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useFetch } from "@hooks/useFetch.js";
 import { useEffect, useState } from "react";
 import Slideshow from "@components/Slideshow/Slideshow";
@@ -9,17 +9,21 @@ import "./SingleProperty.scss";
 const LOGEMENTS__PATH = "/src/data/logements.json";
 
 function SingleProperty() {
-    const id = useParams().id;
+    const { id } = useParams();
+    const navigate = useNavigate();
     const { loading, data: logements, error } = useFetch(LOGEMENTS__PATH);
     const [logement, setLogement] = useState(null);
+
     useEffect(() => {
         if (logements) {
-            const logementCopy = logements.find(
-                (logement) => logement.id === id
-            );
-            setLogement(logementCopy);
+            const foundLogement = logements.find((logement) => logement.id === id);
+            if (foundLogement) {
+                setLogement(foundLogement);
+            } else {
+                navigate("/404");
+            }
         }
-    }, [logements]);
+    }, [logements, id, navigate]);
 
     if (loading) {
         return <div>Chargement en cours...</div>;
@@ -29,36 +33,28 @@ function SingleProperty() {
         return <div>Erreur : {error.message}</div>;
     }
 
-    return (
-        (logement && (
-            <main>
-                <section className="single-property">
-                    <Slideshow
-                        images={logement.pictures}
-                        title={logement.title}
-                    />
-                    <h1 className="single-title">{logement.title}</h1>
-                    <p>{logement.location}</p>
-                    <Rating rating={3} />
-                    <div className="tags">
-                    {logement.tags.map((tag, index) => {
-                        return <span className="tag" key={index}>{tag}</span>
-                    }) }
-                    </div>
-                    <p>{logement.host.name}</p>
-                    <img className="host-name" src={logement.host.picture} alt={logement.host.name} />
-                    <Collapse title="Description">
-                        {logement.description}
-                    </Collapse>
+    if (!logement) {
+        return null;
+    }
 
-                    <Collapse title="Equipements">
-                        {logement.equipments.map((equipment, index) => (
-                                <p key={index}>{equipment}</p>
-                        ))}
-                    </Collapse>
-                </section>
-            </main>
-        )) || <div>Logement introuvable</div>
+    return (
+        <main>
+            <section className="single-property">
+                <Slideshow images={logement.pictures} title={logement.title} />
+                <h1>{logement.title}</h1>
+                <p>{logement.location}</p>
+                <Rating rating={logement.rating} />
+                <img className="host-name" src={logement.host.picture} alt={logement.host.name} />
+                <Collapse title="Description">
+                    {logement.description}
+                </Collapse>
+                <Collapse title="Equipements">
+                    {logement.equipments.map((equipment, index) => (
+                        <p key={index}>{equipment}</p>
+                    ))}
+                </Collapse>
+            </section>
+        </main>
     );
 }
 
