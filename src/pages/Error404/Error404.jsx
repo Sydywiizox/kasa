ef2e583f9cbc8273efcf11c69f056f24aa9505bb
Sydywiizox <andrea_duhamel@outlook.fr>
2025-01-09 16:10:31 +0100

    ajoute main dans about et error

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -3,11 +3,13 @@ import "./Error404.scss";
 
 function Error404() {
   return (
-    <section className="error404">
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <NavLink to="/">Retourner sur la page d'accueil</NavLink>
-    </section>
+    <main>
+      <section className="error404">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+      </section>
+    </main>
   );
 }
 
