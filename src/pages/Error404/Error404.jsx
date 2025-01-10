49ddeb043f762f6be33c2dc078f1e001ed79a5c4
Sydywiizox <andrea_duhamel@outlook.fr>
2025-01-10 14:31:21 +0100

    Navlink to Link in 404

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Error404.scss";
 
 function Error404() {
@@ -7,7 +7,7 @@ function Error404() {
       <section className="error404">
         <h1>404</h1>
         <p>Oups! La page que vous demandez n'existe pas.</p>
-        <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+        <Link to="/">Retourner sur la page d'accueil</Link>
       </section>
     </main>
   );
